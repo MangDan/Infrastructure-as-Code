f98e4a74226846caf19e3430f76a0e108ac3a7cd
mangdan <dan.donghu.kim@gmail.com>
2019-12-26 21:59:04 +0900

    2019-12-26 21:59:04

--- oci/terraform/tmp/apackrsct01/oci-object-storage/datasources.tf
+++
@@ -1,5 +0,0 @@
-data "oci_objectstorage_namespace" "user_namespace" {
-
-  compartment_id = "${var.compartment_ocid}"
-
-}
--- oci/terraform/tmp/apackrsct01/oci-object-storage/output.tf
+++
@@ -1,7 +0,0 @@
-output "dev" {
-  value = "Made by Dan"
-}
-
-output "comments" {
-  value = "This object storage is built by Oracle Resource Manager with Terraform"
-}
